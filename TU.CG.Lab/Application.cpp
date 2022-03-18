ebe1ba331cbbef6637241dba947cf7e2110f3c08
PowerUser <opelastra.mm@gmail.com>
2022-03-18 18:41:28 +0200

    Code for exercise 6.

--- TU.CG.Lab/Application.cpp
+++ TU.CG.Lab/Application.cpp
@@ -18,22 +18,51 @@ void Application::Initialize()
 	constexpr float vertices[] =
 	{
 		// positions          // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
-		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f,   // top left
-	};
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	constexpr unsigned indices[] =
-	{
-		// note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3,   // second triangle
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
 	};
 
 	va = std::make_unique<Graphics::VertexArray>();
 	auto vb = std::make_unique<Graphics::VertexBuffer>(vertices, sizeof vertices);
-	auto eb = std::make_unique<Graphics::ElementBuffer>(indices, 6);
 
 	vb->SetAttributes({
 		{"aPos", Graphics::VertexAttributeType::VEC3F},
@@ -41,8 +70,8 @@ void Application::Initialize()
 	});
 
 	va->SetVertexBuffer(std::move(vb));
-	va->SetElementBuffer(std::move(eb));
 
+	glEnable(GL_DEPTH_TEST);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
@@ -94,16 +123,46 @@ void Application::Render() const
 	boxTexture->BindAndActivate(0);
 	faceTexture->BindAndActivate(1);
 
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-	trans = glm::rotate(trans, window->GetElapsedTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
 	shaderProgram->Use();
-	shaderProgram->SetMat4f("transform", trans);
 
 	va->Bind();
 
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	constexpr glm::vec3 cubePositions[] =
+	{
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f),
+	};
+
+	glm::mat4 view(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+	const glm::mat4 projection = glm::perspective(
+		glm::radians(45.0f),
+		static_cast<float>(window->GetWidth()) / static_cast<float>(window->GetHeight()),
+		0.1f, 100.0f);
+
+	shaderProgram->SetMat4f("view", view);
+	shaderProgram->SetMat4f("projection", projection);
+
+	for (unsigned i = 0; i < 10; ++i)
+	{
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		const float angle = 20.0f * static_cast<float>(i);
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+		shaderProgram->SetMat4f("model", model);
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 
 	va->Unbind();
 	shaderProgram->UnUse();
